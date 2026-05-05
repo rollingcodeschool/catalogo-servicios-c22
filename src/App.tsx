4bf67f4b0204cfef7080d111838632e8dc1d3a84
Emilse Arias <emilsearias@gmail.com>
2026-05-04 21:49:50 -0300

    agregue los link en el footer que direccionen a la pagina del error, y adicione las rutas faltantes en App

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,22 @@ import Footer from "./components/shared/Footer";
 import Menu from "./components/shared/Menu";
 import Error404 from "./components/pages/Error404";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./components/pages/Login";
+import FormularioServicio from "./components/pages/FormularioServicio";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-zinc-950 text-zinc-100 flex flex-col">
         <Menu></Menu>
-        <main className="grow container mx-auto">
+        <main className="grow container mx-auto my-4">
           <Routes>
             <Route path="/" element={<Inicio></Inicio>}></Route>
-            <Route path="/administrador" element={<Administrador></Administrador>}></Route>
+            <Route path="/login" element={<Login></Login>}></Route>
             <Route path="/detalle-servicio" element={<DetalleServicio></DetalleServicio>}></Route>
+            <Route path="/administrador" element={<Administrador></Administrador>}></Route>
+            <Route path="/administrador/crear" element={<FormularioServicio titulo='Crear servicio'/>}></Route>
+            <Route path="/administrador/editar" element={<FormularioServicio titulo='Editar servicio'/>}></Route>
             <Route path="*" element={<Error404></Error404>}></Route>
           </Routes>
         </main>
